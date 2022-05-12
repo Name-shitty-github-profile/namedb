bc2d491b6e2d99b5a5e22237b17ce6303ba1d47f
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-05-12 13:33:35 -0400

    Add files via upload

---
+++ namedb.py
@@ -0,0 +1,165 @@
+from os import listdir, mkdir, rmdir, path; from json import load, dump; from os import remove as delet; from package import convert
+class ValueAlreadyExist(Exception):
+  pass
+class ValueNotExist(Exception):
+  pass
+class CategoryAlreadyExist(Exception):
+  pass
+class CategoryDoesNotExist(Exception):
+  pass
+def search(valuename: str, categorie: str = None):
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('value does not exist')
+  return True
+async def exist(valuename: str, categorie: str = None):
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('value does not exist')
+  return True
+def get(valuename: str, categorie: str = None):
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('Value does not exist')
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json', 'r') as f:
+    data = load(f)
+  return data['value']
+async def select(valuename: str, categorie: str = None):
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('Value does not exist')
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}valuename"}.json', 'r') as f:
+    data = load(f)
+  return data['value']
+def add(valuename: str, value: any, categorie: str = None):
+  if f'{valuename}.json' in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueAlreadyExist('Value already exist')
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json', 'w') as fp:
+    fp.write('{}')
+  data = {}; data['value'] = value
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json', 'w') as fp:
+    dump(data, fp, indent=1)
+  return True
+async def aadd(valuename: str, value: any, categorie: str = None):
+  if f'{valuename}.json' in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueAlreadyExist('Value already exist')
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json', 'w') as fp:
+    fp.write('{}')
+  data = {}; data['value'] = value
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}valuename"}.json', 'w') as fp:
+    dump(data, fp, indent=1)
+  return True
+def edit(valuename: str, value: any, create: bool = False, categorie: str = None):
+  if f'{f"{valuename}" if not categorie else f"{categorie}valuename"}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}') and create: 
+    add(valuename, value) if not categorie else add(valuename, value, categorie)
+    return True
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('Value does not exist')
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json', 'w') as fp:
+    fp.write('{}')
+  data = {}; data['value'] = value
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json', 'w') as fp:
+    dump(data, fp, indent=1)
+  return True
+async def replace(valuename: str, value: any, create: bool = False, categorie: str = None):
+  if f'{f"{valuename}" if not categorie else f"{categorie}valuename"}.json' not in listdir('namedb') and create: 
+    add(valuename, value) if not categorie else add(valuename, value, categorie)
+    return True
+  if f'{f"{valuename}" if not categorie else f"{categorie}valuename"}.json' not in listdir('namedb'): raise ValueNotExist('Value does not exist')
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}valuename"}.json', 'w') as fp:
+    fp.write('{}')
+  data = {}; data['value'] = value
+  with open(f'namedb/{f"{valuename}" if not categorie else f"{categorie}valuename"}.json', 'w') as fp:
+    dump(data, fp, indent=1)
+  return True
+def remove(valuename: str, cache: bool = False, categorie: str = None):
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('Value does not exist')
+  value = get(valuename) if not categorie else get(valuename, categorie)
+  delet(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json')
+  if not cache: return True
+  if f'{valuename}.json' in listdir('namedb/old'): delet(f'namedb/old/{valuename}.json')
+  with open(f'namedb/old/{valuename}.json', 'w') as fp:
+    fp.write('{}')
+  data = {}; data['value'] = value
+  with open(f'namedb/old/{valuename}.json', 'w') as fp:
+    dump(data, fp, indent=1)
+  return True
+async def delete(valuename: str, cache: bool = False, categorie: str = None):
+  if f'{valuename}.json' not in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'): raise ValueNotExist('Value does not exist')
+  delet(f'namedb/{f"{valuename}" if not categorie else f"{categorie}/{valuename}"}.json')
+  if not cache: return True
+  value = get(valuename) if not categorie else get(valuename, categorie)
+  if f'{valuename}.json' in listdir('namedb/old'): delet(f'namedb/old/{valuename}.json')
+  with open(f'namedb/old/{valuename}.json', 'w') as fp:
+    fp.write('{}')
+  data = {}; data['value'] = value
+  with open(f'namedb/old/{valuename}.json', 'w') as fp:
+    dump(data, fp, indent=1)
+  return True
+def createcat(catname: str):
+  pathe = path.join('./namedb/', catname)
+  if path.isdir(pathe): raise CategoryAlreadyExist('Category already exist')
+  mkdir(pathe); return True
+async def acreatecat(catname: str):
+  pathe = path.join('./namedb/', catname)
+  if path.isdir(pathe): raise CategoryAlreadyExist('Category already exist')
+  mkdir(pathe); return True
+def deletecat(catname: str):
+  pathe = path.join('./namedb/', catname)
+  if not path.isdir(pathe): raise CategoryDoesNotExist('Category doesn\'t exist')
+  rmdir(pathe); return True
+async def adeletecat(catname: str):
+  pathe = path.join('./namedb/', catname)
+  if not path.isdir(pathe): raise CategoryDoesNotExist('Category doesn\'t exist')
+  rmdir(pathe); return True
+def prefix(type: str, prefix: str, categorie: str = None):
+  list = []
+  if type == 'value':
+    for file in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'):
+      if file.startswith(prefix):
+        f =  open(f'namedb/{file}', 'r'); data = load(f); list.append(data['value'])
+  elif type == 'name':
+    for file in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'):
+      if file.startswith(prefix):
+        list.append(convert.convert(file))
+  else:
+    raise ValueError('Not a valid type\nTypes : "name", "value"')
+  return list
+async def aprefix(type: str, prefix: str, categorie: str = None):
+  list = []
+  if type == 'value':
+    for file in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'):
+      if file.startswith(prefix):
+        f =  open(f'namedb/{file}', 'r'); data = load(f); list.append(data['value'])
+  elif type == 'name':
+    for file in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'):
+      if file.startswith(prefix):
+        list.append(convert.convert(file))
+  else:
+    raise ValueError('Not a valid type\nTypes : "name", "value"')
+  return list
+def clearprefix(prefix: str, cache: str = False, categorie: str = None):
+  for file in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'):
+    if file.startswith(prefix): remove(convert.convert(file) ,cache) if not categorie else remove(convert.convert(file),cache ,categorie)
+  return True
+async def cleanprefix(prefix: str, cache: str = False, categorie: str = None):
+  for file in listdir(f'namedb{"" if not categorie else f"/{categorie}"}'):
+    if file.startswith(prefix): remove(convert.convert(file) ,cache) if not categorie else remove(convert.convert(file),cache ,categorie)
+  return True
+def restore(valuename: str, categorie: str = None):
+  if f'{valuename}.json' not in listdir('namedb/old'): raise ValueNotExist('value does not exist')
+  with open(f'namedb/old/{valuename}.json', 'r') as fp:
+    data = load(fp)
+  delet(f'namedb/old/{valuename}.json')
+  add(valuename, data['value']) if not categorie else add(valuename, data['value'], categorie)
+  return data['value']
+async def arestore(valuename: str, categorie: str = None):
+  if f'{valuename}.json' not in listdir('namedb/old'): raise ValueNotExist('value does not exist')
+  with open(f'namedb/old/{valuename}.json', 'r') as fp:
+    data = load(fp)
+  delet(f'namedb/old/{valuename}.json')
+  add(valuename, data['value']) if not categorie else add(valuename, data['value'], categorie)
+  return data['value']
+def clearcache():
+  for file in listdir('namedb/old'): delet(f'namedb/old/{file}')
+  return True
+async def cleancache():
+  for file in listdir('namedb/old'): delet(f'namedb/old/{file}')
+  return True
+def transferjson(data, categorie: str = None):
+  for valuename in data:
+    add(valuename, data[valuename]) if not categorie else add(valuename, data[valuename], categorie)
+  return True
+async def atransferjson(data, categorie: str = None):
+  for valuename in data:
+    add(valuename, data[valuename]) if not categorie else add(valuename, data[valuename], categorie)
+  return True
---
+++ readme.md
@@ -0,0 +1,226 @@
+All rights are reserved to Name.
+# get (select)
+parameters : valueName(str), Category(optional(str))<br />
+if value is None value will return None<br />
+```py
+import namedb
+print(f'Price : {namedb.get("price")}')
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.get('a world without taxes')
+except namedb.ValueNotExist:
+  print('Damn it')
+```
+# search (exist)
+parameters : valueName(str), Category(optional(str)) <br />
+if value exist it will return True (bool)<br />
+else False
+```py
+import namedb
+var = namedb.search('price')
+if var:
+  print('Give me your money!')
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.search('a world without taxes')
+except namedb.ValueNotExist:
+  print('Damn it')
+```
+# add (aadd)
+parameters : ValueName(str), Value(Any), Category(optional(str))<br />
+if worked it will return True else False<br />
+```py
+import namedb
+namedb.add('price', 90)
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.add('taxes', 0.19)
+except namedb.ValueAlreadyExist:
+  print('Damn it')
+```
+# edit (aedit)
+parameters : ValueName(str), NewValue(Any), create(optional(bool)), Category(optional(str))<br />
+if worked it will return True else False<br />
+if create is true and the data doesn't exist the data will be added
+```py
+import namedb
+namedb.edit('price', 50)
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.edit('a world without taxes', True)
+except namedb.ValueNotExist:
+  print('Damn it')
+```
+# remove (delete)
+parameters : ValueName(str), cache(optional(bool)), Category(optional(str))<br />
+if worked it will return True else False<br />
+stock the value in the cache if cache is true (false by default)
+```py
+import namedb
+namedb.remove('price')
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.remove('a world without taxes')
+except namedb.ValueNotExist:
+  print('Damn it')
+```
+# transferjson (atransferjson)
+parameters : Data(json), Category(optional(str))<br />
+if worked it will return True else False<br />
+```py
+import namedb
+jsoncode = {
+'price': 90,
+'due': 60
+}
+namedb.transferjson(jsoncode)
+print(f'{namedb.get("price")}')
+```
+# prefix (aprefix)
+parameters : type(value or name), prefix(str), Category(optional(str))<br />
+return a list with all the files that starts with the prefix<br />
+```py
+import namedb
+for costumer in namedb.prefix(type='value', 'costumersdue'):
+  print(f'Due : {costumer}')
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.prefix('hi', 'taxes')
+except ValueError:
+  print('Damn it')
+```
+# clearprefix (cleanprefix)
+parameters : prefix(str), Category(optional(str))<br />
+delete all the values that starts with the prefix<br />
+```py
+import namedb
+namedb.clearprefix('due')
+```
+# restore (arestore)
+parameters : valuename(str), Category(optional(str))<br>
+restore a value from the cache (and return it)
+```py
+import namedb
+namedb.remove('taxes')
+print('too good to be real')
+namedb.restore('taxes')
+```
+error handling<br />
+exemple : 
+```py
+import namedb
+try:
+  namedb.restore('a world without taxes')
+except namedb.ValueNotExist:
+  print('Damn it')
+```
+# clearcache (cleancache)
+parameters : None
+clear the cache
+```py
+import namedb
+namedb.remove('money')
+namedb.clearcache()
+```
+# clearcacheprefix (cleancacheprefix)
+parameters : prefix(str), Category(optional(str))
+clear the cache with a prefix
+```py
+import namedb
+namedb.remove('exemple2286')
+namedb.clearcacheprefix('exemple')
+```
+# createcat (acreatecat)
+Parameters : categoryname(str)<br>
+Create a category
+```py
+import namedb
+namedb.createcat('example')
+```
+# deletecat (adeletecat)
+Parameters : categoryname(str)<br>
+Delete a category
+```py
+import namedb
+namedb.deletecat('example')
+```
+# async
+everything work the same just change the name of the fonction
+example : 
+```py
+import namedb
+async def get_money(user_id: str):
+  value = await namedb.select(f'money{user_id}')
+  return value
+```
+# setup
+install setup.py and run it (answers the questions)<br />
+```bash
+Directory path (example : C:/windows/desktop/my/project/) : C:/windows/Users/Me/Pycharm/project/
+```
+# category
+We highly suggest using category<br>
+## create category
+```py
+import namedb
+namedb.createcat('example')
+```
+## delete category
+```py
+import namedb
+namedb.deletecat('example')
+```
+# migrating from version 1.5
+Category now exist
+example:
+```py
+import namedb
+namedb.createcat('example')
+namedb.add('example', 'working', 'example')
+```
+# example
+```python
+import namedb
+namedb.add('fruits', ['tomato', 'apple'])
+vegetables = namedb.search('vegetables')
+if vegetables:
+  for fruit in namedb.get('fruits'):
+    print(f'Fruits : {fruit}')
+else:
+  for trempette in namedb.prefix(type='value', 't'):
+    print(f'Tempette : {tremepette}')
+namedb.edit('price', namedb.get('price') + 10)
+if namedb.get('paid'):
+  namedb.remove('price')
+else:
+  namedb.edit('price', namedb.get('price') + 1)
+json = {'client': 'Naomie', 'gender': 'female'}
+namedb.transferjson(json)
+```
+# recommandation
+Please use try: and except: if you run a constant code because this errors can make your code stop<br /><br /><br /><br />
+We aren't responsible for data lost.
