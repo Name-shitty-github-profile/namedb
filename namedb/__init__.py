e9a49708fa916865f1d2077290f97a38f6fbf3be
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-13 15:02:49 -0400

    Delete namedb directory

--- namedb/__init__.py
+++
@@ -1,1 +0,0 @@
-from namedb import get, select, search, exist, add, create, edit, replace, remove, delete, searchcat, asearchcat, createcat, acreatecat, deletecat, adeletecat, listcat, alistcat, prefix, aprefix, clearprefix, cleanprefix, restore, arestore, clearcache, cleancache, transferjson, atransferjson
