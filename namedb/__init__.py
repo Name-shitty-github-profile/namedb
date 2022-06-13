9e54b0212af78518cf9cc823448750796465443e
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-13 15:04:03 -0400

    Create __init__.py

---
+++ namedb/__init__.py
@@ -0,0 +1,1 @@
+from namedb import get, select, search, exist, add, create, edit, replace, remove, delete, searchcat, asearchcat, createcat, acreatecat, deletecat, adeletecat, listcat, alistcat, prefix, aprefix, clearprefix, cleanprefix, restore, arestore, clearcache, cleancache, transferjson, atransferjson
